36215779f9d33eb7877a0aab71f780f18382f2a3
Devajit <patar1447@gmail.com>
2024-04-30 11:12:41 +0530

    Chat model class implemented

---
+++ app/src/main/java/com/developer/android/dev/freakycode/androidapp/innertalk/model/Chat.kt
@@ -0,0 +1,7 @@
+package com.developer.android.dev.freakycode.androidapp.innertalk.model
+
+data class Chat(
+    val senderId:String?="",
+    val receiverId:String?="",
+    val message:String?=""
+)

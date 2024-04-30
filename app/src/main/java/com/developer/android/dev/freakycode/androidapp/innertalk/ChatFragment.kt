726694f240b9ef2d43524492a6f46bcede046f08
Devajit <patar1447@gmail.com>
2024-04-30 12:02:01 +0530

    Message Activity make as a Host Activity Host ChatFragment and it UI implemented

---
+++ app/src/main/java/com/developer/android/dev/freakycode/androidapp/innertalk/ChatFragment.kt
@@ -0,0 +1,24 @@
+package com.developer.android.dev.freakycode.androidapp.innertalk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChatFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/developer/android/dev/freakycode/androidapp/innertalk/MessageActivity.kt
+++ app/src/main/java/com/developer/android/dev/freakycode/androidapp/innertalk/MessageActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.developer.android.dev.freakycode.androidapp.innertalk.databinding.ActivityMessageBinding
 
 class MessageActivity : AppCompatActivity() {
@@ -21,9 +23,6 @@ class MessageActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val id = intent.getStringExtra("id")
-        val name = intent.getStringExtra("name")
 
-        binding.userName.text = name.toString()
     }
 }
